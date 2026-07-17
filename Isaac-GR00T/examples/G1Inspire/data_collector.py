625a71456b943894079c992b2d3e16abfa4a7185
smartdude2007 <tommyzhou55@gmail.com>
2026-07-17 15:55:38 -0700

    another

--- Isaac-GR00T/examples/G1Inspire/data_collector.py
+++ Isaac-GR00T/examples/G1Inspire/data_collector.py
@@ -16,24 +16,20 @@ class DataCollector(Node):
     def __init__(self):
         super().__init__('data_collector')
         self.bridge = CvBridge()
-        self.motor_state_subscription = self.create_subscription(LowState, 'motor_states', self.motor_state_callback, 10)
-        self.motor_cmd_subscription = self.create_subscription(LowCmd, 'motor_cmds', self.motor_cmd_callback, 10)
+        self.motor_state_subscription = self.create_subscription(LowState, '/lowstate', self.motor_state_callback, 10)
+        self.motor_cmd_subscription = self.create_subscription(LowCmd, '/lowcmd', self.motor_cmd_callback, 10)
         self.camera_subscription = self.create_subscription(Image, 'camera/image_raw', self.camera_callback, 10)   
-        self.lhand_state_subscription = self.create_subscription(MotorStates, 'lhand_states', self.lhand_state_callback, 10)
-        self.rhand_state_subscription = self.create_subscription(MotorStates, 'rhand_states', self.rhand_state_callback, 10)
-        self.lhand_cmd_subscription = self.create_subscription(MotorCmds, 'lhand_cmds', self.lhand_cmd_callback, 10)
-        self.rhand_cmd_subscription = self.create_subscription(MotorCmds, 'rhand_cmds', self.rhand_cmd_callback, 10)
+        self.lhand_state_subscription = self.create_subscription(MotorStates, '/inspire/state', self.hand_state_callback, 10)
+        self.rhand_cmd_subscription = self.create_subscription(MotorCmds, '/inspire/cmd', self.hand_cmd_callback, 10)
 
         self.control_subscription = self.create_subscription(Bool, '/control', self.control_subscription_callback, 10)
 
-        self.velocity_subscription = self.create_subscription(SportModeCmd, '/velocity', self.base_velocity_callback, 10)
+        self.velocity_subscription = self.create_subscription(SportModeCmd, '/sportmodecmd', self.base_velocity_callback, 10)
 
         self.latest_motor_state = None
         self.latest_motor_cmd = None
-        self.latest_lhand_state = None
-        self.latest_rhand_state = None
-        self.latest_lhand_cmd = None
-        self.latest_rhand_cmd = None
+        self.latest_hand_state = None
+        self.latest_hand_cmd = None
         self.MY_FEATURES = json.load(open("/home/deepak/Isaac-Gr00t-1.7/G1Inspire/features.json"))
         for feat in self.MY_FEATURES.values():
             feat["shape"] = tuple(feat["shape"])
@@ -53,14 +49,10 @@ class DataCollector(Node):
     def base_velocity_callback(self, msg):
         self.latest_base_velocity = np.array([msg.velocity[0], msg.velocity[1], msg.yaw_speed, msg.body_height], dtype=np.float32);
 
-    def lhand_state_callback(self, msg):
-        self.latest_lhand_state = np.array([m.q for m in msg.states], dtype=np.float32);
-    def rhand_state_callback(self, msg):
-        self.latest_rhand_state = np.array([m.q for m in msg.states], dtype=np.float32);
-    def lhand_cmd_callback(self, msg):
-        self.latest_lhand_cmd = np.array([m.q for m in msg.cmds], dtype=np.float32);
-    def rhand_cmd_callback(self, msg):
-        self.latest_rhand_cmd = np.array([m.q for m in msg.cmds], dtype=np.float32);
+    def hand_state_callback(self, msg):
+        self.latest_hand_state = np.array([m.q for m in msg.states], dtype=np.float32);
+    def hand_cmd_callback(self, msg):
+        self.latest_hand_cmd = np.array([m.q for m in msg.cmds], dtype=np.float32);
     def motor_state_callback(self, msg):
         self.latest_motor_state = np.array([msg.motor_state[i].q for i in range(29)], dtype=np.float32);
 
@@ -71,14 +63,13 @@ class DataCollector(Node):
 
     def camera_callback(self, msg):
         if (self.latest_motor_state is None or self.latest_motor_cmd is None or 
-            self.latest_lhand_state is None or self.latest_rhand_state is None 
-            or self.latest_lhand_cmd is None or self.latest_rhand_cmd is None
+            self.latest_hand_state is None or self.latest_hand_cmd is None
             or self.latest_base_velocity is None):
             return
         img = self.bridge.imgmsg_to_cv2(msg, desired_encoding="rgb8")  # (H, W, 3) numpy
         if (self.start_recording):
-            latest_observation_state = np.concatenate((self.latest_motor_state, self.latest_lhand_state, self.latest_rhand_state), axis=0)
-            latest_action = np.concatenate((self.latest_base_velocity,self.latest_motor_cmd, self.latest_lhand_cmd, self.latest_rhand_cmd), axis=0)
+            latest_observation_state = np.concatenate((self.latest_motor_state, self.latest_hand_state), axis=0)
+            latest_action = np.concatenate((self.latest_base_velocity,self.latest_motor_cmd, self.latest_hand_cmd), axis=0)
             self.exporter.add_frame({
                 "observation.state":          latest_observation_state,
                 "action":                     latest_action,
--- README.md
+++ README.md
@@ -1,10 +1,34 @@
 install Groot wholebody controller
 
 ```
+git clone https://github.com/NVlabs/GR00T-WholeBodyControl.git
+
+python -m pip install -e "/home/deepak/Isaac-Gr00t-1.7/GR00T-WholeBodyControl/decoupled_wbc[full]"
+```
 
 
-source unitree ros2
+Source unitree ros2
+```
+source unitree_ros2/cyclonedds_ws/src/unitree/install/setup.bash 
+```
 
-run teleoperation
+ssh into the humanoid and set up communication with the inspire hands
 
-collect data
+```
+ssh unitree@192.168.123.164
+cd dfx_inspire_service/build/
+sudo ./inspire_g1
+```
+
+Run the teleoperation stack
+
+collect data by running the data_collector script
+
+```
+cd Isaac-GR00T/examples/G1Inspire
+python data_collector.py
+```
+
+publish to the control topics to control when to start and stop each episode
+
+```
